23b5d7fa811494e89db3e1e405d3368cf19ae04d
Jakub Konecki <jkonecki@gmail.com>
2014-01-31 23:45:07 +0000

    Added EventStore tab.

---
+++ Glimpse.EventStore/Glimpse.EventStore/UI/EventStoreTab.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glimpse.Core.Extensibility;
+
+namespace Glimpse.EventStore.UI
+{
+    class EventStoreTab : TabBase
+    {
+        public override object GetData(ITabContext context)
+        {
+            var data = new List<object[]>();
+            data.Add(new object[] { "Key", "Value" });
+            data.Add(new object[] { "1", "Sample 1" });
+            data.Add(new object[] { "2", "Sample 2" });
+            data.Add(new object[] { "3", "Sample 3" });
+            
+            return data;
+        }
+
+        public override string Name
+        {
+            get { return "Event Store"; }
+        }
+    }
+}
