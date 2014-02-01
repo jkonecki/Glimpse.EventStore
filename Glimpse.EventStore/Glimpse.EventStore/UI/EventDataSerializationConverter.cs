c7aabb34b2a6215a6133ddeab91328c9bdf09a4b
Jakub Konecki <jkonecki@gmail.com>
2014-02-01 18:13:50 +0000

    Added deserialization of data and metadata. Added an option to hide user password.

---
+++ Glimpse.EventStore/Glimpse.EventStore/UI/EventDataSerializationConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+using EventStore.ClientAPI;
+using Glimpse.Core.Extensibility;
+
+namespace Glimpse.EventStore.UI
+{
+    class EventDataSerializationConverter : SerializationConverter<EventData>
+    {
+        public override object Convert(EventData obj)
+        {
+            var data = Encoding.UTF8.GetString(obj.Data);
+            var metadata = Encoding.UTF8.GetString(obj.Metadata);
+
+            var serializer = new JavaScriptSerializer();
+
+            return new
+            {
+                Data = obj.IsJson ? serializer.DeserializeObject(data) : data,
+                obj.EventId,
+                obj.IsJson,
+                Metadata = obj.IsJson ? serializer.DeserializeObject(metadata) : metadata,
+                obj.Type
+            };
+        }
+    }
+}
---
+++ Glimpse.EventStore/Glimpse.EventStore/UI/UserCredentialsSerializationConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using EventStore.ClientAPI.SystemData;
+using Glimpse.Core.Extensibility;
+
+namespace Glimpse.EventStore.UI
+{
+    class UserCredentialsSerializationConverter : SerializationConverter<UserCredentials>
+    {
+        public const string AppSettingsKey = "Glimpse.EventStore:HideUserCredentialsPassword";
+
+        public override object Convert(UserCredentials obj)
+        {
+            var hidePassword = string.Compare(ConfigurationManager.AppSettings.Get(AppSettingsKey), bool.TrueString, true) == 0;
+
+            return new 
+            {
+                obj.Login, 
+                Password = hidePassword ? "****" : obj.Password
+            };
+        }
+    }
+}
